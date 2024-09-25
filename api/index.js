84498c4e96dbdd7cda14a1af35b975fe34c7e563
NikhilRoyal04 <nikhilchaudhary390@gmail.com>
2024-09-25 14:50:28 +0530

    bug fixed

--- api/index.js
+++ api/index.js
@@ -62,11 +62,11 @@ app.use((err, req, res, next) => {
 });
 
 
-// Start the server
+// // Start the server
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  consoleManager.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   consoleManager.log(`Server is running on port ${PORT}`);
+// });
 
 module.exports = app;
