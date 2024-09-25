24aefcfe52fd6c5d209c0c616a13dd38d29f0d59
NikhilRoyal04 <nikhilchaudhary390@gmail.com>
2024-09-25 13:07:40 +0530

    update

---
+++ api/_api.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("../database/db");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+// Load environment variables
+dotenv.config();
+
+// Initialize the Express app
+const app = express();
+
+// Enable CORS for cross-origin requests
+
+const corsOptions = {
+  origin: "*", // Adjust according to your needs
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true, // Allow credentials such as cookies
+};
+
+app.use(cors(corsOptions));
+
+// Enable cookie-parser
+
+app.use(cookieParser());
+
+// Connect to MongoDB
+connectDB();
+
+// Add middleware
+app.use(express.json());
+
+// Export the initialized app
+module.exports = app;
